6b1f9aabb936020fbfa357303d49fe47e3a372b0
Steven Espinal <stevenjesusespinal@gmail.com>
2023-12-15 02:32:34 -0500

    Sidebar complete

---
+++ app/(browse)/_components/navbar/actions.tsx
@@ -0,0 +1,36 @@
+import { SignInButton, UserButton, currentUser } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { Clapperboard } from "lucide-react";
+
+export const Actions = async () => {
+  const user = await currentUser();
+  return (
+    <div className="flex items-center justify-end gap-x-2 ml-4 lg:ml-0">
+      {!user ? (
+        <SignInButton>
+          <Button size="sm" variant="primary">
+            Login
+          </Button>
+        </SignInButton>
+      ) : null}
+
+      {!!user ? (
+        <div className="flex items-center gap-x-4">
+          <Button
+            size="sm"
+            variant="ghost"
+            className="text-muted-foreground hover:text-primary"
+          >
+            <Link href={`/u/${user.username}`} className="flex">
+              <Clapperboard className="h-5 w-5 lg:mr-2" />
+              <span className="hidden lg:block">Dashboard</span>
+            </Link>
+          </Button>
+        </div>
+      ) : null}
+
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
+};
