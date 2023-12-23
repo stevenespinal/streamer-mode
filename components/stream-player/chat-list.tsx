4b4df3c7f89b572e02ee41f64b838b0f74f70116
Steven Espinal <stevenjesusespinal@gmail.com>
2023-12-23 09:22:24 -0500

    Finished live chat / community logic

--- components/stream-player/chat-list.tsx
+++ components/stream-player/chat-list.tsx
@@ -2,6 +2,7 @@
 
 import { ReceivedChatMessage } from "@livekit/components-react";
 import { ChatMessage } from "./chat-message";
+import { Skeleton } from "@/components/ui/skeleton";
 
 interface ChatListProps {
   messages: ReceivedChatMessage[];
@@ -24,6 +25,14 @@ export const ChatList = ({ messages, isHidden }: ChatListProps) => {
           <ChatMessage key={message.timestamp} data={message} />
         </>
       ))}
+    </div>
+  );
+};
+
+export const ChatListSkeleton = () => {
+  return (
+    <div className="flex h-full items-center justify-center">
+      <Skeleton className="w-1/2 h-6" />
     </div>
   );
 };
--- components/stream-player/video.tsx
+++ components/stream-player/video.tsx
@@ -9,6 +9,7 @@ import {
 import { OfflineVideo } from "./offline-video";
 import { LoadingVideo } from "./loading-video";
 import { LiveVideo } from "./live-video";
+import { Skeleton } from "@/components/ui/skeleton";
 
 interface VideoProps {
   hostName: string;
@@ -31,4 +32,12 @@ export const Video = ({ hostName, hostIdentity }: VideoProps) => {
     content = <LiveVideo participant={participant} />;
   }
   return <div className="aspect-video border-b group relative">{content}</div>;
+};
+
+export const VideoSkeleton = () => {
+  return (
+    <div className="aspect-video border-x border-background">
+      <Skeleton className="h-full w-full rounded-none" />
+    </div>
+  );
 };
