ed2c4d3d102658131f99b7a00ddd7b0f848d286a
Steven Espinal <stevenjesusespinal@gmail.com>
2023-12-24 15:38:34 -0500

    Updating favicon and title

--- lib/search-service.ts
+++ lib/search-service.ts
@@ -74,8 +74,13 @@ export const getSearch = async (term?: string) => {
           },
         ],
       },
-      include: {
+      select: {
         user: true,
+        id: true,
+        name: true,
+        isLive: true,
+        thumbnailUrl: true,
+        updatedAt: true,
       },
       orderBy: [
         {
