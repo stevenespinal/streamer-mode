d0fd78c7f2241923f7d3476e0ae9025b66b3418d
Steven Espinal <stevenjesusespinal@gmail.com>
2023-12-24 04:14:44 -0500

    Finished clean up for potential security risks in selecting entire stream;

--- components/stream-player/index.tsx
+++ components/stream-player/index.tsx
@@ -12,9 +12,29 @@ import { Header, HeaderSkeleton } from "./header";
 import { InfoCard } from "./info-card";
 import { AboutCard } from "./about-card";
 
+type CustomStream = {
+  id: string;
+  isChatEnabled: boolean;
+  isChatFollowersOnly: boolean;
+  isLive: boolean;
+  isChatDelayed: boolean;
+  name: string;
+  thumbnailUrl: string | null;
+};
+
+type CustomUser = {
+  id: string;
+  username: string;
+  bio: string | null;
+  stream: CustomStream | null;
+  imageUrl: string;
+  _count: {
+    followedBy: number;
+  };
+};
 interface StreamPlayerProps {
-  user: User & { stream: Stream | null; _count: { followedBy: number } };
-  stream: Stream;
+  user: CustomUser;
+  stream: CustomStream;
   isFollowing: boolean;
 }
 export const StreamPlayer = ({
